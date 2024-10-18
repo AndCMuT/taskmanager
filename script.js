d9bac21ca096bf9348015d42de2151af15252e9e
Andrey_Smetanin <cmutyxa@gmail.com>
2024-10-18 08:07:58 +0300

    lesson massives js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let age = parseInt(prompt());
-console.log(age > 18 ? "Доступ разрешен" : "Доступ запрещен");
+const n = parseInt(prompt());
+let arr = [];
+for (i = 0; i < n; i++) {
+      arr.push(prompt().split(' '));
+}
+let sum = 0;
+for (let j = 0; j < n; j++){
+      for (let k = 0; k < n; k++) {
+            sum = sum + parseInt(arr[j][k]);
+      }
+}
+let average = sum / (n*n);
+for (let j = 0; j < n; j++){
+      for (let k = 0; k < n; k++) {
+            if (arr[j][k] >= average) {
+                  console.log(arr[j][k]);
+            }
+      }
+}
